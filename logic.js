f0f6b0db620398062e7d79d6165c3674df60ca3b
ignacioDias <ignadiasg@gmail.com>
2024-10-08 20:26:00 -0300

    completed

---
+++ logic.js
@@ -0,0 +1,80 @@
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
+console.log("conectado!");
+
+const operations = document.querySelector("#operations");
+const output = document.querySelector("#output");
+
+operations.addEventListener("click", (event) => {
+    if(output.innerText == "" || isNaN(output.innerText)) {
+        output.innerText = "Error, put a number first! Start again";
+        return;
+    }
+    firstNumber = parseInt(output.innerText, 10);
+    output.innerText = '';
+    operator = event.target.id;
+
+});
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", () => {
+    if(output.innerText == "" || isNaN(output.innerText) || firstNumber == null || operator == null) {
+        output.innerText = "Error, start again!!";
+        return;
+    }
+    secondNumber = parseInt(output.innerText, 10);
+    output.innerText = '';
+    switch(operator) {
+        case "add":
+            firstNumber += secondNumber;
+            output.innerText = firstNumber;
+            break;
+        case "subscract":
+            firstNumber -= secondNumber;
+            output.innerText = firstNumber;
+            break;
+        case "division":
+            if(secondNumber != 0) {
+                firstNumber /= secondNumber;
+                output.innerText = firstNumber;
+            } else {
+                output.innerText = "Can't divide by 0!";
+            }
+            break;
+        case "product":
+            firstNumber *= secondNumber;
+            output.innerText = firstNumber;
+            break;
+        case "pow":
+            firstNumber **= secondNumber;
+            output.innerText = firstNumber;
+            break;
+    }
+    secondNumber = null;
+    operator = null;
+});
+
+const numbers = document.querySelector("#numbers");
+numbers.addEventListener("click", (event) => {
+    if (event.target.tagName !== "BUTTON")
+        return;
+    if(event.target.id == "clear") {
+        firstNumber = null;
+        secondNumber = null;
+        operator = null;
+        output.innerText = "";
+        return;
+    }
+    if(event.target.id == "delete") {
+        if(!isNaN(output.innerText))
+            output.innerText = output.innerText.slice(0, -1); 
+        return;
+    }
+    if(output.innerText == "" || isNaN(output.innerText))
+        output.innerText = event.target.id;
+    else
+        output.innerText += event.target.id;
+});
+
